563ec62cb7198cde5ef10c004231f9d6306f6487
Eric Torstenson <eric.s.torstenson@vumc.org>
2026-06-22 16:34:55 -0500

    The version() expects the project name defined inside the pyproject.toml file

--- src/tweaver/__init__.py
+++ src/tweaver/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("tweaver")
+    __version__ = version("term-weaver")
 except PackageNotFoundError:
     # package is not installed
     __version__ = "0.0.0"
